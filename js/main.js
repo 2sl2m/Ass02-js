e75cb67e6618dd8da79956be95c7be4bafabfdcf
2sl2m <152660478+2sl2m@users.noreply.github.com>
2023-12-11 16:02:28 -0800

    frist commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var coments = ["Hello, world!", "I love coding!", "JavaScript is awesome!"
+];
+
+
+
+
+
+function getInputsData(){
+   document.getElementById("demo").textContent= coments 
+}
+
+
+
+
+
